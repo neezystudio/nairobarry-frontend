ba865777267080483a6c2fedf48a2731b1554c9b
neezystudio <victorneez^@gmail.com>
2021-10-04 13:56:19 +0300

    login

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -4,9 +4,18 @@ import { Buttons, Column, Container, Image, Link, MenuBars, Rows, ScrollLinks, W
 import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
 function LoginScreen(){
     const [modalOpen, setModalOpen] = useState(false);
+    const [navbar, setNavbar] = useState(false);
     const handleModal = () => {
         setModalOpen(!modalOpen);
     }
+    const changeBackground = () => {
+        if(window.scrollY >= 80) {
+            setNavbar(true);
+        } else {
+            setNavbar(false);
+        }
+    }
+    window.addEventListener('scroll', changeBackground);
     return (
         <div className= "loginScreen">
     <div className= "loginScreen__background">
@@ -24,8 +33,22 @@ function LoginScreen(){
     <div className= "loginScreen__body">
         <>
         <h1>
-       Unlimited films,TV programmes and more
+       NAIROBBERY
         </h1>
+        <h2>
+            watch anywhere. cancel at any time
+        </h2>
+        <h3>Ready to watch, enter your email to create your account or restart your membership</h3>
+        <div classname= "loginScreen__input">
+            <form>
+                <input type="email" placeholder="Email Address" />
+                <button className="loginScreen__getStarted"
+                onClick={handleModal}
+                >
+                    GET STARTED
+                </button>
+            </form>
+        </div>
         </>
     </div>
     </div>
